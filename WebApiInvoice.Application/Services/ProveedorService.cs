51ed7f2ee61698decef2031b0e2d2ef36935834d
Adriana Cristaldo <adrianacristaldo96@gmail.com>
2025-07-10 12:19:51 -0300

    creacion de proveedorService

---
+++ WebApiInvoice.Application/Services/ProveedorService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiInvoice.Application.Interfaces;
+using WebApiInvoice.Domain.Interfaces;
+using WebApiInvoice.Domain.Models;
+
+namespace WebApiInvoice.Application.Services
+{
+    public class ProveedorService : IProveedorService
+    {
+        
+    }
+
+}
