3c08653c4faf695a4475387c1417189bd2fa5a89
Adriana Cristaldo <adrianacristaldo96@gmail.com>
2025-07-11 00:53:11 -0300

    completar desarrollo de ProveedoresController

--- WebApiInvoice.WebApi/Controllers/ProveedoresController.cs
+++ WebApiInvoice.WebApi/Controllers/ProveedoresController.cs
@@ -8,10 +8,58 @@ using WebApiInvoice.Domain.Models;
 
 namespace WebApiInvoince.WebApi.Controllers
 {
-    
+    [ApiController]
+    [Route("api/proveedores")]
+
     public class ProveedoresController : ControllerBase
     {
+        IProveedorService _service;
 
+        public ProveedoresController(IProveedorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<Proveedor> GetAll()
+        {
+            return _service.SelectAll();
+        }
+
+        /// <summary>
+        /// Obtener un proveedor x id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Proveedor), StatusCodes.Status200OK)]
+        public IActionResult GetById(int id)
+        {
+            return new OkObjectResult(_service.SelectById(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Proveedor), StatusCodes.Status201Created)]
+        public IActionResult CreateProveedor(Proveedor proveedor)
+        {
+            return new CreatedResult("default", _service.Add(proveedor));
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult DeleteProveedor(int id)
+        {
+            return new OkObjectResult(_service.Delete(id));
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool),StatusCodes.Status200OK)]
+        public IActionResult UpdateProveedor (Proveedor model)
+        {
+            return new OkObjectResult(_service.Update( model));
+        }
     }
+
+    
         
 }
