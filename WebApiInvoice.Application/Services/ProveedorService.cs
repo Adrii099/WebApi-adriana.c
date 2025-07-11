0bf8ee4e61eb4041dc1e2386c737c7d281dcf243
Adriana Cristaldo <adrianacristaldo96@gmail.com>
2025-07-11 11:25:33 -0300

    coreccion de llaves en ProveedorService capa aplicacion

--- WebApiInvoice.Application/Services/ProveedorService.cs
+++ WebApiInvoice.Application/Services/ProveedorService.cs
@@ -11,51 +11,58 @@ namespace WebApiInvoice.Application.Services
 {
     public class ProveedorService : IProveedorService
     {
-        
+        readonly IProveedorRepository _repository;
     
         public ProveedorService(IProveedorRepository repository)
         {
-            readonly IProveedorRepository _repository;
-            public ProveedorService(IProveedorRepository repository)
+            _repository = repository;
+        }
+            
+
+        public bool Add(Proveedor model)
+        {
+            if (string.IsNullOrEmpty(model.Name))
             {
-                _repository = repository;
+                return false;
+            }
+            if (model.Price <= 0)
+            {
+                return false;
             }
 
-            public bool Add(Proveedor model)
+            return _repository.Create(model) != 0;
+        }
+            
+        public bool Update(Proveedor model)
+        {
+            if (model.Id == 0)
             {
-                if (string.IsNullOrEmpty(model.Name))
-                    return false;
-                if (model.Price <= 0)
-                    return false;
-
-                return _repository.Create(model) != 0;
+                return false;
             }
-            public bool Update(Proveedor model)
+            return _repository.Update(model, model.Id) != 0;
+        }
+
+        public bool Delete(int id)
+        {
+            if (id == 0)
             {
-                if (model.Id == 0)
-                    return false;
-                return _repository.Update(model, model.Id) != 0;
+                return false;
             }
+            return _repository.Delete(id) != 0;
+        }
 
-            public bool Delete(int id)
-            {
-                if (id == 0)
-                    return false;
-                return _repository.Delete(id) != 0;
-            }
+        public Proveedor SelectById(int id)
+        {
+            if (id == 0) return null;
 
-            public Proveedor SelectById(int id)
-            {
-                if (id == 0) return null;
-
-                return _repository.GetById(id); 
-            }
+            return _repository.GetById(id); 
+        }
 
             public List<Proveedor> SelectAll()
             {
                 return _repository.Get();
             }
-        }
     }
-
+     
 }
+
