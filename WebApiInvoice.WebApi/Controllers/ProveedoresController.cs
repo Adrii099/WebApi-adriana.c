04e6499c5e1ef9018e5df9f3e0e7fdb4542274d1
Adriana Cristaldo <adrianacristaldo96@gmail.com>
2025-07-10 23:45:35 -0300

    ProveedoresController en la capa WebApiInvoice

---
+++ WebApiInvoice.WebApi/Controllers/ProveedoresController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiInvoice.Application.Interfaces;
+using WebApiInvoice.Domain.Models;
+
+namespace WebApiInvoince.WebApi.Controllers
+{
+    
+    public class ProveedoresController : ControllerBase
+    {
+
+    }
+        
+}
